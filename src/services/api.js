2352c722cbf9c1783ac8d4d22c6dbd39c341d9ea
Lukman Nur Hakim <luqmannur33@gmail.com>
2025-05-04 08:58:09 +0700

    update api call

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://203.194.112.226/api/';
 
 const api = axios.create({
   baseURL: API_URL,
